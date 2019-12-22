bdf656886a6d9cdc9c87f7cf99649ecc6b433a49
shibuya-r <50525690+shibuya-r@users.noreply.github.com>
2019-12-22 23:14:10 +0900

    test-commit

--- Invader-M/Assets/Scenes/BulletController.cs
+++ Invader-M/Assets/Scenes/BulletController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour
 {
+
+    public GameObject explosionPrefab;   //爆発エフェクトのPrefab
+
     void Update()
     {
         transform.Translate(0, 0.2f, 0);
@@ -11,5 +14,13 @@ public class BulletController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // 爆発エフェクトを生成する	
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(coll.gameObject);
+        Destroy(gameObject);
     }
 }
